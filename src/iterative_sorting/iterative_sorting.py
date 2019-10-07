ed7fa39f6cbd739be2212cb21fccffb78f7a5c02
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-07 16:09:33 +0200

    added new double loop solution for bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -15,15 +15,15 @@ def selection_sort( arr ):
 # TO-DO:  implement the Bubble Sort function below
 
 # SOLUTION WITH WHILE LOOP
-# def bubble_sort(arr):
-#   check = True
-#   while check:
-#     check = False
-#     for index in range(0, len(arr) - 1):
-#       if arr[index] > arr[index + 1]:
-#         arr[index], arr[index + 1] = arr[index + 1], arr[index]
-#         check = True
-#   return arr
+def bubble_sort(arr):
+  check = True
+  while check:
+    check = False
+    for index in range(0, len(arr) - 1):
+      if arr[index] > arr[index + 1]:
+        arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        check = True
+  return arr
 
 # SOLUTION WITH DOUBLE LOOP
 def bubble_sort(arr):
@@ -33,8 +33,15 @@ def bubble_sort(arr):
         arr[j], arr[j + 1] = arr[j + 1], arr[j]
   return arr
 
+#DIFFERENT SOLUTION WITH DOUBLE LOOP
+def bubble_sort(arr):
+  for i in range(len(arr)):
+    for j in range(i + 1, len(arr)):
+      if arr[i] > arr[j]:
+        arr[i], arr[j] = arr[j], arr[i]
+  return arr
 
-# add recursive bubble sort for deeper understanding of algorithm:
+:
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
